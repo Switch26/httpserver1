bb4e1ff0917509342e99b20abf8217266beb03bc
Switch26 <sergueivinnitskii@gmail.com>
2022-08-06 17:50:44 -0400

    added error handling

--- main.go
+++ main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 )
 
 func main() {
-	fmt.Printf("running Main()")
+	fmt.Printf("running Main()\n")
 	http.HandleFunc("/", getRoot)
 	http.HandleFunc("/hello", getHello)
-	//err := http.ListenAndServe(":3333", nil)
+	err := http.ListenAndServe(":3333", nil)
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("server closed\n")
+	} else if err != nil {
+		fmt.Printf("error starting server %s\n", err)
+		os.Exit(1)
+	}
 }
 
 func getRoot(w http.ResponseWriter, r *http.Request) {
